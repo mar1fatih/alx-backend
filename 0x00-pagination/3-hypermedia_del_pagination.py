bdcc4132eeeb530aad63e103db52f1cc2e67b6f7
mar1fatih <marouanefatih631@gmail.com>
2024-08-19 00:01:59 +0000

    task3

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -5,7 +5,7 @@ Deletion-resilient hypermedia pagination
 
 import csv
 import math
-from typing import List, Dict, Union
+from typing import List, Dict
 
 
 class Server:
@@ -39,8 +39,7 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None,
-                        page_size: int = 10) -> Dict[str, Union[int, List]]:
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         assert index >= 0 and index < len(self.__dataset)
         _dict: Dict = {
             'index': index,
