9db4fd9690254e14dc96d133364767bde0e05a59
mar1fatih <marouanefatih631@gmail.com>
2024-08-21 17:35:10 +0100

    3. LRU Caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,61 +1,37 @@
 #!/usr/bin/env python3
 """ lifo_cache """
+from collections import OrderedDict
 
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-def old_num(LRU):
-    """ find the min """
-    min = None
-    for k in LRU.keys():
-        if min is None:
-            min = LRU[k]
-        if LRU[k] < min:
-            min = LRU[k]
-    return min
-
-
 class LRUCache(BaseCaching):
     """ LRUCache class """
-    count = 0
-    old = 1
-    LRU = dict()
 
     def __init__(self):
         """ constructor """
         super().__init__()
+        self.cache_data = OrderedDict(self.cache_data)
 
     def put(self, key, item):
         """ put method to take data using LRU method """
         if key and item:
             if key in self.cache_data.keys():
                 self.cache_data[key] = item
-                self.count += 1
-                self.LRU[key] = self.count
-                self.old = old_num(self.LRU)
+                self.cache_data.move_to_end(key)
                 return
             if len(self.cache_data) == BaseCaching.MAX_ITEMS:
-                for k in self.LRU.keys():
-                    if self.LRU[k] == self.old:
-                        print('DISCARD:', k)
-                        self.cache_data.pop(k)
-                        self.LRU.pop(k)
-                        self.old = old_num(self.LRU)
-                        break
+                poped = self.cache_data.popitem(last=False)
+                print('DISCARD:', poped[0])
                 self.cache_data[key] = item
-                self.count += 1
-                self.LRU[key] = self.count
                 return
             self.cache_data[key] = item
-            self.count += 1
-            self.LRU[key] = self.count
 
     def get(self, key):
         """ get method to get data """
         if key and key in self.cache_data.keys():
-            self.count += 1
-            self.LRU[key] = self.count
+            self.cache_data.move_to_end(key)
             return self.cache_data[key]
         else:
             return None
