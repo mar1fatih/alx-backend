99613ece5f89a433d148d08c9e1770aa806f8346
mar1fatih <marouanefatih631@gmail.com>
2024-08-28 00:33:21 +0100

    task 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -25,7 +25,7 @@ def get_locale():
 @app.route('/')
 def home():
     """home app"""
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
